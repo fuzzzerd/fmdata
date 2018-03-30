1c04500381968b55483aaef19e425315b86769bf
Nate Bross <Nate.B@WizAD.INT>
2018-03-30 13:44:36 -0500

    initial find request plus authentication

---
+++ src/FMREST/FindRequest.cs
@@ -0,0 +1,58 @@
+namespace FMREST
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class FindRequest
+    {
+        /// <summary>
+        /// The find request dictionary.
+        /// </summary>
+        [JsonProperty("query")]
+        public IEnumerable<Dictionary<string, string>> Query { get; set; }
+
+        /// <summary>
+        /// Maximum number of records to return for this request.
+        /// </summary>
+        [JsonProperty("range")]
+        public int Range { get; set; }
+
+        /// <summary>
+        /// The number of records to skip before returning records for this request.
+        /// </summary>
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        /// <summary>
+        /// The sort fields and directions for this request.
+        /// </summary>
+        [JsonProperty("sort")]
+        public Sort[] Sort { get; set; }
+
+        /// <summary>
+        /// Create a find request from Json
+        /// </summary>
+        /// <param name="json">The incomming Json data to deserialize.</param>
+        /// <returns>An instance of the FindRequest object from the provided Json string.</returns>
+        public static FindRequest FromJson(string json) => JsonConvert.DeserializeObject<FindRequest>(json);
+
+        /// <summary>
+        /// Convert the this instance to Json.
+        /// </summary>
+        /// <returns>Json serialization of this instance.</returns>
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+
+    public partial class Sort
+    {
+        [JsonProperty("fieldName")]
+        public string FieldName { get; set; }
+        [JsonProperty("sortOrder")]
+        public string SortOrder { get; set; }
+    }
+}
---
+++ src/FMREST/FmsAuthResponse.cs
@@ -0,0 +1,10 @@
+namespace FMREST
+{
+    public class FmsAuthResponse
+    {
+        public string Token { get; set; }
+        public string Layout { get; set; }
+        public string ErrorCode { get; set; }
+        public string Reseult { get; set; }
+    }
+}
