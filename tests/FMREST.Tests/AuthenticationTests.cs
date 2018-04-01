2adb1af6c8809c801d67fa9db5ee0adca0262dcd
Nate Bross <Nate.B@WizAD.INT>
2018-04-01 14:26:31 -0500

    Started test class

--- tests/FMREST.Tests/AuthenticationTests.cs
+++ tests/FMREST.Tests/AuthenticationTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace FMREST.Tests
 {
-    public class UnitTest1
+    public class AuthenticationTests
     {
         [Fact]
-        public void Test1()
+        public void TestAuthShouldSucceed()
         {
 
         }
