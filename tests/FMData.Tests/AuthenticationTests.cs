ddbae23136e398153877546d092e226da71f1a3e
Nate Bross <Nate.B@WizAD.INT>
2018-04-04 14:09:54 -0500

    Couple more unit tests.

--- tests/FMData.Tests/AuthenticationTests.cs
+++ tests/FMData.Tests/AuthenticationTests.cs
@@ -8,7 +8,7 @@ namespace FMREST.Tests
     public class AuthenticationTests
     {
         [Fact]
-        public void TestAuthShouldSucceed()
+        public void NewUp_DataClient_ShouldBeAuthenticated()
         {
             var mockHttp = new MockHttpMessageHandler();
 
@@ -24,6 +24,46 @@ namespace FMREST.Tests
             var fdc = new FMDataClient(mockHttp.ToHttpClient(), server, file, user, pass, layout);
 
             Assert.True(fdc.IsAuthenticated);
+        }
+
+        [Fact]
+        public async Task RefreshToken_ShouldGet_NewToken()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            var server = "http://localhost";
+            var file = "test-file";
+            var user = "unit";
+            var pass = "test";
+            var layout = "layout";
+
+            mockHttp.When($"{server}/fmi/rest/api/auth/{file}")
+                    .Respond("application/json", DataApiResponses.SuccessfulAuthentication("someOtherToken"));
+
+            var fdc = new FMDataClient(mockHttp.ToHttpClient(), server, file, user, pass, layout);
+
+            var response = await fdc.RefreshTokenAsync("integration", "test", "someLayout");
+            Assert.Equal("someOtherToken", response.Token);
+        }
+
+        [Theory]
+        [InlineData("", "test", "layout")]
+        [InlineData("integration", "", "layout")]
+        [InlineData("integration", "test", "")]
+        public async Task RefreshToken_Requires_AllParameters(string user, string pass, string layout)
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            var server = "http://localhost";
+            var file = "test-file";
+
+            mockHttp.When($"{server}/fmi/rest/api/auth/{file}")
+                    .Respond("application/json", DataApiResponses.SuccessfulAuthentication("someOtherToken"));
+
+            // pass in actual values here since we DON'T want this to blow up on constructor 
+            var fdc = new FMDataClient(mockHttp.ToHttpClient(), server, file, "user", "pass", "layout");
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await fdc.RefreshTokenAsync(user, pass, layout));
         }
     }
 }
