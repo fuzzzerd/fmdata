338f0ba31946ab14e3f42bdef969c1cea680ab57
Nate Bross <nate@brosstribe.com>
2018-04-15 19:26:43 -0500

    two tests using package

--- tests/FMData.Tests/CreateRequestTests.cs
+++ tests/FMData.Tests/CreateRequestTests.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FMData.Requests;
-using RichardSzalay.MockHttp;
+//using RichardSzalay.MockHttp;
 using Xunit;
 
 namespace FMData.Tests
