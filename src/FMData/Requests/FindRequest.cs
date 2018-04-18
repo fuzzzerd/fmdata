ce1fdb8f0ecc0c4114836cd8be8f94cb7a51cbe2
Nate Bross <nate@brosstribe.com>
2018-04-18 17:08:45 -0500

    Test coverage to insure find requests are serialized correctly for fms

--- src/FMData/Requests/FindRequest.cs
+++ src/FMData/Requests/FindRequest.cs
@@ -8,6 +8,32 @@ using Newtonsoft.Json.Converters;
 
 namespace FMData.Requests
 {
+    /// <summary>
+    /// Ensure that numbers are written out to the json object as strings for FileMaker Data API compatibility.
+    /// </summary>
+    /// <remarks>Source: https://stackoverflow.com/a/39526179/86860</remarks>
+    sealed class FormatNumbersAsTextConverter : JsonConverter
+    {
+        public override bool CanRead => false;
+        public override bool CanWrite => true;
+        public override bool CanConvert(Type type) => type == typeof(int);
+
+        public override void WriteJson(
+            JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            int number = (int)value;
+            writer.WriteValue(number.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public override object ReadJson(JsonReader reader, Type type, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    /// <summary>
+    /// The object that contains the parameters to serialize for a find request.
+    /// </summary>
     public partial class FindRequest<TRequestType> : RequestBase
     {
         /// <summary>
@@ -20,19 +46,19 @@ namespace FMData.Requests
         /// Maximum number of records to return for this request.
         /// </summary>
         [JsonProperty("range")]
-        public int Range { get; set; }
+        public int Range { get; set; } = 100; // default 
 
         /// <summary>
         /// The number of records to skip before returning records for this request.
         /// </summary>
         [JsonProperty("offset")]
-        public int Offset { get; set; }
+        public int Offset { get; set; } = 0; // default
 
         /// <summary>
         /// The sort fields and directions for this request.
         /// </summary>
         [JsonProperty("sort")]
-        public Sort[] Sort { get; set; }
+        public IEnumerable<Sort> Sort { get; set; }
 
         /// <summary>
         /// Create a find request from Json
@@ -45,7 +71,15 @@ namespace FMData.Requests
         /// Convert the this instance to Json.
         /// </summary>
         /// <returns>Json serialization of this instance.</returns>
-        public string ToJson() => JsonConvert.SerializeObject(this);
+        public string ToJson() => JsonConvert.SerializeObject(
+            this,
+            Newtonsoft.Json.Formatting.None,
+            new JsonSerializerSettings 
+            { 
+                NullValueHandling = NullValueHandling.Ignore, 
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                Converters = { new FormatNumbersAsTextConverter() }
+            });
     }
 
     public partial class Sort
