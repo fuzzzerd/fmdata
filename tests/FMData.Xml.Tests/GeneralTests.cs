305245039fbe7cdb229767741b13073b6741ce6c
Nate Bross <nate@brosstribe.com>
2018-05-09 15:36:14 -0500

    Test project and dependencies for xml

---
+++ tests/FMData.Xml.Tests/GeneralTests.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace FMData.Tests
+{
+    public class GeneralTests
+    {
+        //private FindRequest<User> FindReq => new FindRequest<User>()
+        //{
+        //    Query = new List<User>()
+        //    {
+        //        new User()
+        //        {
+        //            Id =1
+        //        }
+        //    },
+        //    Layout = "layout"
+        //};
+
+        //[Fact]
+        //public void FindRequest_Numbers_ShouldSerialize_ToStrings_ForFileMaker()
+        //{
+        //    //arrange 
+        //    var r = FindReq;
+
+        //    // act
+        //    var json = r.ToJson();
+
+        //    //assert
+        //    Assert.Contains("\"Id\":\"1\"", json);
+        //}
+
+        //[Fact]
+        //public void FindRequest_Numbers_ShouldNotSerialize_ToNumbers()
+        //{
+        //    //arrange 
+        //    var r = FindReq;
+
+        //    // act
+        //    var json = r.ToJson();
+
+        //    //assert
+        //    Assert.DoesNotContain("\"Id\":1", json);
+        //}
+    }
+}
