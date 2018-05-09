1bf4382f756dd0d2b245fd9a312c69c9b1065c2a
Nate Bross <nate@brosstribe.com>
2018-05-09 12:52:15 -0500

    everything compiles, tests pass!

--- src/FMData.Rest/Responses/IFindResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FMData.Rest.Responses
-{
-    public interface IFindResponse<TResponseType>
-    {
-        IEnumerable<RecordBase<TResponseType, Dictionary<string, string>>> Data { get; set; }
-    }
-}
