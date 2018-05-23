4a7263cf3144251bf1608b3d1d372d71463c4d95
Nate Bross <nate@brosstribe.com>
2018-05-23 11:35:17 -0500

    Fixed all warnings.

--- src/FMData.Rest/Requests/CreateRequest.cs
+++ src/FMData.Rest/Requests/CreateRequest.cs
@@ -2,8 +2,15 @@ using Newtonsoft.Json;
 
 namespace FMData.Rest.Requests
 {
+    /// <summary>
+    /// Create Request Wrapper
+    /// </summary>
+    /// <typeparam name="T">The type to create.</typeparam>
     public class CreateRequest<T> : RequestBase, ICreateRequest<T>
     {
+        /// <summary>
+        /// The field data for the create request.
+        /// </summary>
         [JsonProperty("fieldData")]
         public T Data { get; set; }
     }
