9fa5ab386f98815175242ba4d8081746bbd393cc
Nate Bross <nate@brosstribe.com>
2018-05-23 10:55:29 -0500

    Add scripts to requests.

--- src/FMData.Rest/Requests/RequestBase.cs
+++ src/FMData.Rest/Requests/RequestBase.cs
@@ -2,7 +2,10 @@ using Newtonsoft.Json;
 
 namespace FMData.Rest
 {
-    public class RequestBase
+    /// <summary>
+    /// Base Request Implemenation
+    /// </summary>
+    public class RequestBase : IFileMakerRequest
     {
         /// <summary>
         /// Name of the layout to run the request on
@@ -15,6 +18,39 @@ namespace FMData.Rest
         /// </summary>
         [JsonProperty("layout.response")]
         public string ResponseLayout { get; set; }
+
+        /// <summary>
+        /// Request Script. Occurs post request, and post sort.
+        /// </summary>
+        [JsonProperty("script")]
+        public string Script { get; set; }
+        /// <summary>
+        /// Request Script Parameter.
+        /// </summary>
+        [JsonProperty("script.param")]
+        public string ScriptParameter { get; set; }
+
+        /// <summary>
+        /// Pre-request script. Runs after going to the layout in the request, but before the API request takes place.
+        /// </summary>
+        [JsonProperty("script.prerequest")]
+        public string PreRequestScript { get; set; }
+        /// <summary>
+        /// Pre-request script parameter.
+        /// </summary>
+        [JsonProperty("script.prerequest.param")]
+        public string PreRequestScriptParameter { get; set; }
+
+        /// <summary>
+        /// Pre-sort request. Occurs after the pre-request and the api request but before the sort has occured.
+        /// </summary>
+        [JsonProperty("script.presort")]
+        public string PreSortScript { get; set; }
+        /// <summary>
+        /// Pre-sort script parameter.
+        /// </summary>
+        [JsonProperty("script.presort.param")]
+        public string PreSortScriptParameter { get; set; }
 
         /// <summary>
         /// JSON Convert the current object to a string for passing out to the API.
--- src/FMData.Xml/Requests/FindRequest.cs
+++ src/FMData.Xml/Requests/FindRequest.cs
@@ -10,6 +10,12 @@ namespace FMData.Xml.Requests
         public int Limit { get; set; }
         public IEnumerable<ISort> Sort { get; set; }
         public string ResponseLayout { get; set; }
+        public string Script { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string ScriptParameter { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string PreRequestScript { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string PreRequestScriptParameter { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string PreSortScript { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string PreSortScriptParameter { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
         public string SerializeRequest()
         {
