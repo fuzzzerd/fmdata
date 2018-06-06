73239230feaebb3981483f977d69e1c6e27f3c74
Nate Bross <nate@brosstribe.com>
2018-06-06 12:57:47 -0500

    Tidy up a few messed up comments.

--- src/FMData.Rest/Responses/AuthResponse.cs
+++ src/FMData.Rest/Responses/AuthResponse.cs
@@ -1,12 +1,24 @@
 ﻿namespace FMData.Rest.Responses
 {
+    /// <summary>
+    /// Authentication Response
+    /// </summary>
     public class AuthResponse : BaseResponse
     {
+        /// <summary>
+        /// Wrapper for the Response object from FileMaker API.
+        /// </summary>
         public AuthResponseResult Response { get; set; }
     }
-
+    
+    /// <summary>
+    /// Response result containing the token.
+    /// </summary>
     public class AuthResponseResult
     {
+        /// <summary>
+        /// The token provided as a result of the authentication request.
+        /// </summary>
         public string Token { get; set; }
     }
 }
