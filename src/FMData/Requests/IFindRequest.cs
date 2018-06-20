f5201e3554e10b0e3b07ddb474cc843a697e732d
Nate Bross <nate@brosstribe.com>
2018-06-20 12:43:32 -0500

    Tidy up code comments for primary projects.

--- src/FMData/Requests/IFindRequest.cs
+++ src/FMData/Requests/IFindRequest.cs
@@ -2,12 +2,28 @@
 
 namespace FMData
 {
+    /// <summary>
+    /// Find Request Interface
+    /// </summary>
+    /// <typeparam name="TRequestType">The type of object to find.</typeparam>
     public interface IFindRequest<TRequestType> : IFileMakerRequest
     {
+        /// <summary>
+        /// The Offset (number of records to skip)
+        /// </summary>
         int Offset { get; set; }
+        /// <summary>
+        /// The Limit (number of records to return)
+        /// </summary>
         int Limit { get; set; }
 
+        /// <summary>
+        /// The object to use as query parameters.
+        /// </summary>
         IEnumerable<TRequestType> Query { get; set; }
+        /// <summary>
+        /// The sort options for this request.
+        /// </summary>
         IEnumerable<ISort> Sort { get; set; }
     }
 }
--- src/FMData/Responses/IResponse.cs
+++ src/FMData/Responses/IResponse.cs
@@ -4,14 +4,29 @@ using System.Text;
 
 namespace FMData
 {
+    /// <summary>
+    /// General Response from FileMaker Server API
+    /// </summary>
     public interface IResponse
     {
+        /// <summary>
+        /// The messages returned by the request.
+        /// </summary>
         IEnumerable<ResponseMessage> Messages { get; set; }
     }
 
+    /// <summary>
+    /// Response Message Wrapper (required by response format for serialization reasons)
+    /// </summary>
     public class ResponseMessage
     {
+        /// <summary>
+        /// The Message Code (typically an error, zero for OK)
+        /// </summary>
         public string Code { get; set; }
+        /// <summary>
+        /// The Message's message
+        /// </summary>
         public string Message { get; set; }
     }
 }
