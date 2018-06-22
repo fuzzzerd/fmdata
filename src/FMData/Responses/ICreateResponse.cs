64cdad8f0416e1de12236a9c326e31b77d07ad59
Nate Bross <nate@brosstribe.com>
2018-06-22 09:33:59 -0500

    Add modid and scripterror per actual response from api.

--- src/FMData/Responses/ICreateResponse.cs
+++ src/FMData/Responses/ICreateResponse.cs
@@ -20,5 +20,15 @@
         /// Record Id that was created.
         /// </summary>
         public int RecordId { get; set; }
+
+        /// <summary>
+        /// Modification Id of the record.
+        /// </summary>
+        public int ModId { get; set; }
+
+        /// <summary>
+        /// Script Error (if any)
+        /// </summary>
+        public int ScriptError { get; set; }
     }
 }
