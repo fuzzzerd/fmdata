0958f7752c031836f5a5dc9d0176f7e31a1ee5f3
Nate Bross <nate@brosstribe.com>
2018-08-06 14:46:54 -0500

    test coverage for model mapping.

--- tests/FMData.Xml.Tests/TestModels/Art.cs
+++ tests/FMData.Xml.Tests/TestModels/Art.cs
@@ -1,10 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace FMData.Xml.Tests.TestModels
 {
     [Table("layout")]
     public class Art
     {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Style { get; set; }
+        public int length { get; set; }
+    }
+
+    [DataContract(Name ="layout")]
+    public class ArtDataCM
+    {
+        [DataMember(Name ="alt-Title")]
         public string Title { get; set; }
         public string Artist { get; set; }
         public string Style { get; set; }
