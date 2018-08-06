b5625915cd5a6276be513471544cc5e981b73eab
Nate Bross <nate@brosstribe.com>
2018-08-06 15:12:17 -0500

    Updated xml edit requests.

---
+++ src/FMData.Xml/Responses/EditResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FMData.Xml.Responses
+{
+    /// <summary>
+    /// Create response instance
+    /// </summary>
+    public class EditResponse : IEditResponse
+    {
+        /// <summary>
+        /// The response object from the create request.
+        /// </summary>
+        public ActionResponse Response { get; set; }
+
+        /// <summary>
+        /// The messages from this response.
+        /// </summary>
+        public IEnumerable<ResponseMessage> Messages { get; set; }
+    }
+}
---
+++ tests/FMData.Xml.Tests/EditRequestTests.cs
@@ -0,0 +1,45 @@
+using FMData.Xml.Tests.TestModels;
+using RichardSzalay.MockHttp;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FMData.Xml.Tests
+{
+    public class EditRequestTests
+    {
+        string server = "http://localhost";
+        string file = "test-file";
+        string user = "unit";
+        string pass = "test";
+        string layout = "layout";
+
+        [Fact]
+        public async Task EditRecord_ShouldMatch_SentData()
+        {
+            // arrange 
+            var nameToMatch = "fuzzzerd";
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When(HttpMethod.Post, $"{server}/fmi/xml/fmresultset.xml")
+                .WithPartialContent($"-edit")
+                .WithPartialContent(nameToMatch)
+                .Respond(HttpStatusCode.OK, "application/xml", "");
+
+            var fdc = new FileMakerXmlClient(mockHttp.ToHttpClient(), server, file, user, pass);
+
+            var mtoEdit = new User()
+            {
+                Id = 3,
+                Name = nameToMatch
+            };
+
+            var response = await fdc.EditAsync(5, mtoEdit);
+
+            Assert.NotNull(response);
+            Assert.Contains(response.Messages, r => r.Message == "OK");
+        }
+    }
+}
