ebf337985fd5a83afd36af216a154c91c37895ea
Nate Bross <nate@brosstribe.com>
2018-08-21 15:42:33 -0500

    Started some work towards #19

---
+++ src/FMData/ContainerForAttribute.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace FMData
+{
+    /// <summary>
+    /// FileMaker record with Dictionary as field data and portal data.
+    /// </summary>
+    public class ContainerDataForAttribute : System.Attribute
+    { 
+        /// <summary>
+        /// The name of the container field to load data from.
+        /// </summary>
+        public string ContainerField {get;set;}
+        
+        /// <summary>
+        /// Constructor for Attribute
+        /// </summary>
+        /// <param name="containerField">The name of the container field to load data from.</param>
+        public ContainerDataForAttribute(string containerField)
+        {
+            ContainerField = containerField;
+        }
+    }
+}
