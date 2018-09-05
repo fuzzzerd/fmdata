af90a73243accaf4f811db6e9ba3e3728d9532f7
Nate Bross <nate@brosstribe.com>
2018-09-04 22:03:48 -0500

    Added unit test for process container data.

--- tests/FMData.Rest.Tests/FindRequestTests.cs
+++ tests/FMData.Rest.Tests/FindRequestTests.cs
@@ -383,6 +383,42 @@ namespace FMData.Rest.Tests
             Assert.Equal(recordId, response.FileMakerRecordId);
         }
 
+        [Fact]
+        public async Task GetByRecordId_ShouldHaveContainerWithContainerDataFor()
+        {
+            // arrange
+            object FMrecordIdMapper(ContainerFieldTestModel o, int id) => o.FileMakerRecordId = id;
+            var mockHttp = new MockHttpMessageHandler();
+
+            var server = "http://localhost";
+            var file = "test-file";
+            var user = "unit";
+            var pass = "test";
+            var layout = "Users";
+            var recordId = 26;
+
+            mockHttp.When(HttpMethod.Post, $"{server}/fmi/data/v1/databases/{file}/sessions")
+                           .Respond("application/json", DataApiResponses.SuccessfulAuthentication());
+
+            var b64String = System.IO.File.ReadAllText("ResponseData\\b64-string.dat");
+            var bytes = Convert.FromBase64String(b64String);
+            var b64= new ByteArrayContent(bytes);
+
+            mockHttp.When(HttpMethod.Get, $"{server}/some-data-path")
+                .Respond(b64);
+
+            mockHttp.When(HttpMethod.Get, $"{server}/fmi/data/v1/databases/{file}/layouts/{layout}/records/{recordId}")
+                .Respond("application/json", DataApiResponses.SuccessfulGetByIdWithContainer(recordId, $"{server}/some-data-path"));
+
+            var fdc = new FileMakerRestClient(mockHttp.ToHttpClient(), server, file, user, pass);
+
+            // act
+            var response = await fdc.GetByFileMakerIdAsync<ContainerFieldTestModel>(recordId, FMrecordIdMapper);
+
+            // assert
+            Assert.Equal(bytes, response.SomeContainerFieldData);
+        }
+
 
         [Fact]
         public async Task SendAsync_Dictionary_WithPortals_ShouldHaveData()
