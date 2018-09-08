0a979a032cd34644d196fe404bd563686ec347f0
Nate Bross <nate@brosstribe.com>
2018-09-08 14:31:16 -0500

    Completed implementation for #22

---
+++ src/FMData.Xml/Requests/DeleteRequest.cs
@@ -0,0 +1,24 @@
+namespace FMData.Xml.Requests
+{
+    /// <summary>
+    /// Create Request Wrapper
+    /// </summary>
+    /// <typeparam name="T">The type to create.</typeparam>
+    public class DeleteRequest : RequestBase, IDeleteRequest
+    {
+        /// <summary>
+        /// RecordId
+        /// </summary>
+        public int RecordId { get; set; }
+
+        /// <summary>
+        /// Serialize the request to the format needed for filemaker to accept it.
+        /// </summary>
+        /// <returns>String representation of the request.</returns>
+        public override string SerializeRequest()
+        {
+            var requestContent = $"-delete&-lay={Layout}&-recid={RecordId}";
+            return requestContent;
+        }
+    }
+}
