3c622bd0ca06d7106f3bf87e888ad239477d8be2
Nate Bross <nate@brosstribe.com>
2018-09-08 16:21:13 -0500

    initial portal work

---
+++ tests/FMData.Rest.Tests/TestModels/PortalModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FMData.Rest.Tests.TestModels
+{
+    [DataContract(Name ="the-layout")]
+    public class PortalModel
+    {
+        public string ES_ONE { get; set; }
+        public int ES_TWO { get; set; }
+        public int ES_UTCDTO { get; set; }
+
+        [PortalData("action")]
+        public IEnumerable<Action> Actions { get; set; }
+    }
+
+    [DataContract(Name ="actionLayout")]
+    public class Action
+    {
+        [DataMember(Name ="ID")]
+        public int ID { get; set; }
+        [DataMember(Name = "Action")]
+        public string ActionName { get; set; }
+    }
+}
