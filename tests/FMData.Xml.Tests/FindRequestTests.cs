12d46f89caa016724520ed3eb0ec599b0ad6a14f
Nate Bross <nate@brosstribe.com>
2018-09-09 21:40:47 -0500

    Completed #7 support in xml library

--- tests/FMData.Xml.Tests/FindRequestTests.cs
+++ tests/FMData.Xml.Tests/FindRequestTests.cs
@@ -66,5 +66,31 @@ namespace FMData.Xml.Tests
             var first = response.First();
             Assert.Equal(19, first.length);
         }
+
+        [Fact]
+        public async Task FindRecord_WithPortal_Should_MatchLength()
+        {
+            // arrange 
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When(HttpMethod.Post, $"{server}/fmi/xml/fmresultset.xml")
+                .WithPartialContent("-find")
+                .WithPartialContent(Uri.EscapeDataString("Spring in Giverny 3"))
+                .Respond(HttpStatusCode.OK, "application/xml", XmlResponses.GrammarSample_fmresultset);
+
+            var fdc = new FileMakerXmlClient(mockHttp.ToHttpClient(), server, file, user, pass);
+
+            var findRequest = new ArtWithPortal()
+            {
+                Title = "Spring in Giverny 3"
+            };
+
+            var response = await fdc.FindAsync(findRequest);
+
+            Assert.NotEmpty(response);
+            var first = response.First();
+            Assert.Equal(19, first.length);
+            Assert.Equal("Chicago", first.Locations.First().Location);
+        }
     }
 }
