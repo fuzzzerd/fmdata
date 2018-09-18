bcaa2496086813392edde8aad74bd71ef8ca89e4
Nate Bross <nate@brosstribe.com>
2018-09-17 23:59:26 -0500

    Fix #40 -- break into two classes

--- src/FMData.Rest/Responses/AuthResponse.cs
+++ src/FMData.Rest/Responses/AuthResponse.cs
@@ -11,14 +11,5 @@
         public AuthResponseResult Response { get; set; }
     }
     
-    /// <summary>
-    /// Response result containing the token.
-    /// </summary>
-    public class AuthResponseResult
-    {
-        /// <summary>
-        /// The token provided as a result of the authentication request.
-        /// </summary>
-        public string Token { get; set; }
-    }
+    
 }
