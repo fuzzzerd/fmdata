783b48fbfb3f2be8ef17fe47224770e70aa2f1f5
Nate Bross <nate@brosstribe.com>
2018-09-18 00:01:31 -0500

    Fix for #39

---
+++ src/FMData.Rest/Requests/Sort.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMData.Rest.Requests
+{
+    /// <summary>
+    /// Data Class For Sort
+    /// </summary>
+    public partial class Sort : ISort
+    {
+        /// <summary>
+        /// The name of the sort field.
+        /// </summary>
+        [JsonProperty("fieldName")]
+        public string FieldName { get; set; }
+        /// <summary>
+        /// Sort direction (ascend/descend).
+        /// </summary>
+        [JsonProperty("sortOrder")]
+        public string SortOrder { get; set; }
+    }
+}
