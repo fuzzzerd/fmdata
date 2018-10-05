8ba5636bd80b3c10e154025177aa6871903ec2e8
Nate Bross <nate@brosstribe.com>
2018-10-04 22:37:58 -0500

    VS got a little screwy and removed a bit more than expected.

--- tests/FMData.Rest.Tests/EditRequestTests.cs
+++ tests/FMData.Rest.Tests/EditRequestTests.cs
@@ -1,4 +1,12 @@
+using FMData.Rest;
+using FMData.Rest.Requests;
 using FMData.Rest.Tests.TestModels;
+using RichardSzalay.MockHttp;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace FMData.Rest.Tests
 {
