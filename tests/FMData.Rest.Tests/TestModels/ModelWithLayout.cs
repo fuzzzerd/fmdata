470f7b2a582c036345117419f323454455ef7fd0
Nate Bross <nate@brosstribe.com>
2018-10-04 22:07:26 -0500

    Split create record tests into two files.

---
+++ tests/FMData.Rest.Tests/TestModels/ModelWithLayout.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace FMData.Rest.Tests.TestModels
+{
+    [DataContract(Name ="Somelayout")]
+    public class ModelWithLayout
+    {
+        public string Name { get; set; }
+        public string AnotherField { get; set; }
+    }
+}
